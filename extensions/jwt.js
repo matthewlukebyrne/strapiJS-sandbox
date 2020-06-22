299bc855923658c5b875043cec5cae00fa661e0c
Matt <matthewlukebyrne@gmail.com>
2020-06-23 00:07:20 +0100

    strapiJs-quicklook

---
+++ extensions/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '79827276-6fdc-477c-87d0-f802377a53e8'
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cdb745f0-1530-46f2-92da-4c38419000a5'
+};
